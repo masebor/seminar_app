147c5e5f41d67bd6e4fb26ec385e5301a92b4b75
masebor <masebor@posteo.de>
2024-08-24 14:26:11 +0200

    improved alertdialog style and colors extra file

--- lib/styles/theme/theme.dart
+++ lib/styles/theme/theme.dart
@@ -1,84 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:seminar_app/styles/app_colors.dart';
 
 class AppTheme {
   AppTheme._();
 
-  static final Color _lightPrimaryColor = Colors.blueGrey.shade50;
-  static final Color _lightPrimaryVariantColor = Colors.blueGrey.shade800;
-  static final Color _lightOnPrimaryColor = Colors.blueGrey.shade400;
-  static const Color _lightTextColorPrimary = Colors.black;
-  static const Color _appbarColorLight = Color.fromARGB(255, 156, 46, 175);
-
-  static final Color _darkPrimaryColor = Colors.blueGrey.shade900;
-  static const Color _darkPrimaryVariantColor = Colors.black;
-  static const Color _darkOnPrimaryColor = Color.fromARGB(255, 119, 161, 182);
-  static const Color _darkTextColorPrimary = Colors.white;
-  static final Color _appbarColorDark = Colors.blueGrey.shade800;
-
-  static const Color _iconColorLight = Colors.white;
-  static const Color _iconColorDark = Color.fromARGB(255, 78, 144, 177);
-
-  static const Color _accentColorLight = Color.fromRGBO(69, 223, 223, 1);
-  static const Color _accentColorDark = Color.fromRGBO(222, 198, 76, 1);
-
-  static const TextStyle _lightHeadingText = TextStyle(
-    color: _lightTextColorPrimary,
-    fontFamily: "Poppins",
+  static const TextStyle lightHeadingText = TextStyle(
+    color: AppColors.lightTextColorPrimary,
+    fontFamily: "Verdana",
     fontSize: 20,
     fontWeight: FontWeight.bold,
   );
 
-  static const TextStyle _lightBodyText = TextStyle(
-    color: _lightTextColorPrimary,
-    fontFamily: "Poppins",
+  static const TextStyle lightBodyText = TextStyle(
+    color: AppColors.lightTextColorPrimary,
+    fontFamily: "Verdana",
     fontWeight: FontWeight.bold,
     fontSize: 14,
   );
 
-  static const TextTheme _lightTextTheme = TextTheme(
-    displayLarge: _lightHeadingText,
-    bodyLarge: _lightBodyText,
+  static const TextTheme lightTextTheme = TextTheme(
+    displayLarge: lightHeadingText,
+    bodyLarge: lightBodyText,
   );
 
-  static final TextStyle _darkThemeHeadingTextStyle = _lightHeadingText.copyWith(
-    color: _darkTextColorPrimary,
+  static final TextStyle darkThemeHeadingTextStyle = lightHeadingText.copyWith(
+    color: AppColors.darkTextColorPrimary,
   );
 
-  static final TextStyle _darkThemeBodyeTextStyle = _lightBodyText.copyWith(
-    color: _darkTextColorPrimary,
+  static final TextStyle darkThemeBodyeTextStyle = lightBodyText.copyWith(
+    color: AppColors.darkTextColorPrimary,
   );
 
-  static final TextTheme _darkTextTheme = TextTheme(
-    displayLarge: _darkThemeHeadingTextStyle,
-    bodyLarge: _darkThemeBodyeTextStyle,
+  static final TextTheme darkTextTheme = TextTheme(
+    displayLarge: darkThemeHeadingTextStyle,
+    bodyLarge: darkThemeBodyeTextStyle,
   );
 
   static final ThemeData lightTheme = ThemeData(
-      scaffoldBackgroundColor: _lightPrimaryColor,
-      appBarTheme: const AppBarTheme(
-        color: _appbarColorLight,
-        iconTheme: IconThemeData(color: _iconColorLight),
-      ),
-      bottomAppBarTheme: const BottomAppBarTheme(color: _appbarColorLight),
-      colorScheme: ColorScheme.light(
-          primary: _lightPrimaryColor,
-          onPrimary: _lightOnPrimaryColor,
-          secondary: _accentColorLight,
-          primaryContainer: _lightPrimaryVariantColor),
-      textTheme: _lightTextTheme);
+    scaffoldBackgroundColor: AppColors.lightPrimaryColor,
+    appBarTheme: const AppBarTheme(
+      color: AppColors.appbarColorLight,
+      iconTheme: IconThemeData(color: AppColors.iconColorLight),
+    ),
+    bottomAppBarTheme: const BottomAppBarTheme(color: AppColors.appbarColorLight),
+    colorScheme: ColorScheme.light(
+      primary: AppColors.lightPrimaryColor,
+      onPrimary: AppColors.lightOnPrimaryColor,
+      secondary: AppColors.accentColorLight,
+      primaryContainer: AppColors.lightPrimaryVariantColor,
+    ),
+    textTheme: lightTextTheme,
+  );
 
   static final ThemeData darkTheme = ThemeData(
-      scaffoldBackgroundColor: _darkPrimaryColor,
-      appBarTheme: AppBarTheme(
-        color: _appbarColorDark,
-        iconTheme: const IconThemeData(color: _iconColorDark),
-      ),
-      bottomAppBarTheme: BottomAppBarTheme(color: _appbarColorDark),
-      colorScheme: ColorScheme.dark(
-        primary: _darkPrimaryColor,
-        secondary: _accentColorDark,
-        onPrimary: _darkOnPrimaryColor,
-        primaryContainer: _darkPrimaryVariantColor,
-      ),
-      textTheme: _darkTextTheme);
+    scaffoldBackgroundColor: AppColors.darkPrimaryColor,
+    appBarTheme: AppBarTheme(
+      color: AppColors.appbarColorDark,
+      iconTheme: const IconThemeData(color: AppColors.iconColorDark),
+    ),
+    bottomAppBarTheme: BottomAppBarTheme(color: AppColors.appbarColorDark),
+    colorScheme: ColorScheme.dark(
+      primary: AppColors.darkPrimaryColor,
+      secondary: AppColors.accentColorDark,
+      onPrimary: AppColors.darkOnPrimaryColor,
+      primaryContainer: AppColors.darkPrimaryVariantColor,
+    ),
+    textTheme: darkTextTheme,
+  );
 }
