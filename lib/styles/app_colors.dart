e2a33f6e52686e96d248874c2ec21a849d97fe8e
masebor <masebor@posteo.de>
2024-08-25 11:19:28 +0200

    fix: theme colors adjusted

--- lib/styles/app_colors.dart
+++ lib/styles/app_colors.dart
@@ -3,17 +3,19 @@ import 'package:flutter/material.dart';
 class AppColors {
   AppColors._();
 
-  static final Color lightPrimaryColor = Colors.blueGrey.shade50;
-  static final Color lightPrimaryVariantColor = Colors.blueGrey.shade800;
-  static final Color lightOnPrimaryColor = Colors.blueGrey.shade400;
-  static const Color lightTextColorPrimary = Colors.black;
+  static final Color primaryColorLight = Colors.blueGrey.shade50;
+  static final Color primaryVariantColorLight = Colors.blueGrey.shade800;
+  static final Color onPrimaryColorLight = Colors.blueGrey.shade400;
+  static const Color secondaryColorLight = Color.fromARGB(255, 40, 73, 233);
+  static const Color textColorPrimaryLight = Colors.black;
   static const Color appbarColorLight = Color.fromARGB(255, 156, 46, 175);
 
-  static final Color darkPrimaryColor = Colors.blueGrey.shade900;
-  static const Color darkPrimaryVariantColor = Colors.black;
-  static const Color darkOnPrimaryColor = Color.fromARGB(255, 119, 161, 182);
-  static const Color darkTextColorPrimary = Colors.white;
-  static final Color appbarColorDark = Colors.blueGrey.shade800;
+  static final Color primaryColorDark = Colors.blueGrey.shade900;
+  static const Color primaryVariantColorDark = Colors.black;
+  static const Color onPrimaryColorDark = Color.fromARGB(255, 119, 161, 182);
+  static const Color secondaryColorDark = Color.fromARGB(255, 38, 199, 151);
+  static const Color textColorPrimaryDark = Colors.white;
+  static const Color appbarColorDark = Color.fromARGB(255, 61, 81, 91);
 
   static const Color approveColor = Color.fromARGB(255, 93, 198, 167);
   static const Color disapproveColor = Color.fromARGB(255, 188, 87, 79);
@@ -21,6 +23,6 @@ class AppColors {
   static const Color iconColorLight = Colors.white;
   static const Color iconColorDark = Color.fromARGB(255, 78, 144, 177);
 
-  static const Color accentColorLight = Color.fromRGBO(69, 223, 223, 1);
-  static const Color accentColorDark = Color.fromRGBO(222, 198, 76, 1);
+  static const Color selectedItemColorLight = Color.fromRGBO(42, 195, 195, 1);
+  static const Color selectedItemColorDark = Color.fromRGBO(222, 198, 76, 1);
 }
