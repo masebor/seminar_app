8f4435cd05a28d3b8c82d60d57a55ae652224da9
masebor <masebor@posteo.de>
2024-08-25 08:40:03 +0200

    feature: global animated keys and source url list

---
+++ lib/constants/global_animated_keys.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class GlobalAnimatedKeys {
+  static final introPageKey = GlobalKey<AnimatedListState>();
+}
--- lib/constants/source_url_lists.dart
+++ lib/constants/source_url_lists.dart
@@ -1,12 +1,14 @@
-// class SourceUrlLists {
-//   SourceUrlLists._();
-
-List<String> testUrlList = [
-  "https://flutter.dev/",
-  "https://pub.dev/",
-  "https://dart.dev/",
-  "https://medium.com/vijay-r/flutter-url-launcher-9a5346c93216",
-];
+import 'package:seminar_app/utils/custom_card.dart';
 
 
-// }
+class SourceUrlLists {
+
+    static final List<CustomCard> oldIntroList = [
+    const CustomCard(content: "Android", source: "https://www.android.com/"),
+    const CustomCard(content: "iOS", source: "https://www.apple.com/de/ios/"),
+    const CustomCard(
+        content: "long Html",
+        source:
+            "https://www.google.com/search?q=how+to+make+text+selectable+influtter&client=firefox-b-d&sca_esv=33b519b0d73ed9c3&sca_upv=1&ei=DuLJZrbaEOKhi-gPzoDhyAM&ved=0ahUKEwi2mdns342IAxXi0AIHHU5AGDkQ4dUDCA8&uact=5&oq=how+to+make+text+selectable+influtter&gs_lp=Egxnd3Mtd2l6LXNlcnAiJWhvdyB0byBtYWtlIHRleHQgc2VsZWN0YWJsZSBpbmZsdXR0ZXIyCRAAGIAEGBMYDTIIEAAYgAQYogQyCBAAGIAEGKIESOwKUO8CWIcKcAF4AZABAJgBf6ABsAWqAQM1LjK4AQPIAQD4AQGYAgegAukEwgIKEAAYsAMY1gQYR8ICBRAhGKABwgIIEAAYCBgNGB6YAwCIBgGQBgiSBwM1LjKgB8QU&sclient=gws-wiz-serp"),
+  ];
+}
